175e58ef5d2121d45341c5f3ff9cf62f6c9edfb5
Ryu <kpleeae@connect.ust.hk>
2024-05-12 11:48:46 +0800

    add division

--- algo/div.asm
+++ algo/div.asm
@@ -1,4 +1,4 @@
-# perform mutlply operation
+# perform division
 .data
 msg1: .asciiz  "Enter the first number: "
 msg2: .asciiz "Enter the second number: "
@@ -37,27 +37,52 @@ main:
     addi $t0, $zero, 0 
     addi $t1, $zero, 32 
     add $t2, $zero, $zero
-    add $t3, $zero, $s1
+    add $t3, $zero, $s0
+
+    li $t4, 1
+    sll $t4, $t4, 31
+    and $t4, $t4, $t3
+    srl $t4, $t4, 31 # carry bit
+
+    sll $t2, $t2, 1
+    sll $t3, $t3, 1
+
 
 i_for_loop_start:
     beq $t0, $t1, i_for_loop_end
 
-    and $t4, $t3, 1
-    beq $t4, $zero, addition_end
+    sub $t4, $t2, $s1 
 
-    add $t2, $s0, $t2
+    # check if the number negative
+    srl $t5, $t4, 31
+    bne $t5, $zero, subtraction_end
 
-addition_end:
-    and $t4, $t2, 1 # used to shift this bit to lo register
-    srl $t2, $t2, 1
-    srl $t3, $t3, 1
+    move $t2, $t4
 
+subtraction_end:
+    li $t4, 1
     sll $t4, $t4, 31
-    or $t3, $t3, $t4
+    and $t4, $t4, $t3
+    srl $t4, $t4, 31 # carry bit
+
+    sll $t2, $t2, 1
+    sll $t3, $t3, 1
+
+    or $t2, $t2, $t4 
+
+    bne $t5, $zero, setlsb_end
+
+    # set least signifant bit to 1
+    addi $t3, $t3, 1
+
+setlsb_end:
 
     addi $t0, $t0, 1 # i+= 1
     j i_for_loop_start
 i_for_loop_end:
+
+    # adjust reminder
+    srl $t2, $t2, 1
 
    
 
